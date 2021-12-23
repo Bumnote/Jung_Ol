5d9578f578aef46226484449bf7f18d0734a32a3
YongBum Kim <dt10002@naver.com>
2021-12-23 14:33:23 +0900

    2021.12.23

---
+++ 정올_변수와입력/732_형성평가_5.py
@@ -0,0 +1,3 @@
+n1 = float(input("yard? "))
+answer = n1 * 91.44
+print(n1, "yard", "=", answer, "cm")
---
+++ 정올_함수_1/961_자가진단_1.py
@@ -0,0 +1,7 @@
+def solution(n):
+    for i in range(n):
+        print("~!@#$^&*()_+|")
+
+
+n = int(input())
+solution(n)
